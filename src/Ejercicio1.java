58f151e24659a91d4e1dde92c3a29fda62c894cb
carlos-a-pv <carlosa.perezv@uqvirtual.edu.co>
2023-10-06 16:32:30 -0500

    cambio de nombre, del archivo

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         System.out.println("primero numero(velocidad): ");
