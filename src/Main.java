5ccfe2edb5a7bd10fd4300a46b98c1ec926f83bb
carlos-a-pv <carlosa.perezv@uqvirtual.edu.co>
2023-10-06 16:29:39 -0500

    solucion ejercicio #1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("primero numero(velocidad): ");
+        int v = entrada.nextInt();
+        System.out.println("Segundo numero(tiempo): ");
+        int t = entrada.nextInt();
+
+        System.out.println(calculateDobleDisplacement(v, t));
+    }
+    public static int calculateDobleDisplacement(int a, int b){
+        return 2*(a*b);
+    }
+}
